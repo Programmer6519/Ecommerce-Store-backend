ae8a2d6fcd3c67640cc8a62c00a15821545c5470
Programmer6519 <faizansaith401@gmail.com>
2026-03-25 11:47:52 +0500

    removed bugs

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("../public/temp"));
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
