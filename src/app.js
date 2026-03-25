4ded97318c901e2e1484c3294f0ee8566ff831a6
Programmer6519 <faizansaith401@gmail.com>
2026-03-25 12:51:40 +0500

    changes in vecel.json for deploment

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("../public/temp"));
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("HELLO THE SERVER IS RUNNING");
 });
 app.use("/api/v1/product", verifyJWT, productRouter);
